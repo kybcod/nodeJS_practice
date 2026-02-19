13c48e36f76a737e946865c5c4ce5630c23f2612
김예빈 <kyb-0215@naver.com>
2026-02-19 13:36:22 +0900

    express 정적 파일을 서비스 하는 방법

--- app.js
+++ app.js
@@ -2,8 +2,14 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.use(express.static('public')); //정적파일(public 디렉토리에 정적파일 넣어놓기)
+
+app.get('/route', (req, res) => {
+  res.send('<h1>Hello Router!</h1>, <img src="/dddd.jpg">')
+})
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('<h1>Hello World!</h1>')
 })
 
 app.listen(port, () => {
